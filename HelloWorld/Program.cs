4082f0c3dd81fe6cc1e8eddda3c8871d1fc1e0cc
btphan <phan.buu@gmail.com>
2016-10-07 20:39:37 -0700

    Second commit

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -8,7 +8,7 @@ namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("This is Hello World!");
             Console.ReadLine();
 
         }
