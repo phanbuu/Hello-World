54523ea849e2dfbd0d2fd563b129ec0c67cbb762
Buu Phan <btphan@mdanderson.org>
2017-06-21 15:10:42 -0500

    From work computer, change 40 to 140

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld {
     class Program {
         static void Main(string[] args) {
 
-            Console.WriteLine(Adding.Adding2Integers(40, 40));
+            Console.WriteLine(Adding.Adding2Integers(140, 40));
             Console.ReadLine();
 
         }
